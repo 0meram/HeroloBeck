34cb9f9566de112c17332670658de39421eab2b1
omer ram <0meram95@gmail.com>
2021-12-21 11:29:45 +0200

    seconde commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+// require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 4000;
+// const cors = require("cors");
+
+app.use(express.json());
+// app.use(cors());
+// app.use("/search", require("./routes/search"));
+
+app.get("/", (req, res) => {
+	res.send(`<h1>Herolo Server</h1>`);
+});
+
+app.listen(PORT, () => {
+	console.log(`im ready to work on port ${PORT}`);
+});
