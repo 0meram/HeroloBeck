3e6836fa7c23e3bde8f01c989a035b92bfcd5cc3
omer ram <0meram95@gmail.com>
2021-12-21 12:02:30 +0200

    search

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require("express");
-// require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 4000;
-// const cors = require("cors");
+const cors = require("cors");
 
 app.use(express.json());
-// app.use(cors());
-// app.use("/search", require("./routes/search"));
+app.use(cors());
+app.use("/search", require("./routes/search"));
 
 app.get("/", (req, res) => {
-	res.send(`<h1>Herolo Server de hello there</h1>`);
+	res.send(`<h1>Herolo Server let me automation</h1>`);
 });
 
 app.listen(PORT, () => {
